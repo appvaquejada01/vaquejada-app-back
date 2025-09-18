b5492ae4433239fc8f16d733af622627f29a211f
AbnerBittencourt <abnerrocha00@gmail.com>
2025-09-18 01:01:20 -0300

    [master] refactor: reorganize user exception classes for improved readability

--- src/modules/user/exceptions/user.exception.ts
+++ src/modules/user/exceptions/user.exception.ts
@@ -1,7 +1,18 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
+export class UserNotFoundException extends HttpException {
+  constructor() {
+    super('User not found', HttpStatus.NOT_FOUND);
+  }
+}
 export class CpfAlreadyInUseException extends HttpException {
   constructor() {
     super('CPF number already in use', HttpStatus.CONFLICT);
+  }
+}
+
+export class EmailAlreadyInUseException extends HttpException {
+  constructor() {
+    super('Email address already in use', HttpStatus.CONFLICT);
   }
 }
