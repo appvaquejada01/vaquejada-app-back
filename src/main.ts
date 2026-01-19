9696ec459ae5a35485a17c7891424b0e02e7664e
AbnerBittencourt <abnerrocha00@gmail.com>
2026-01-19 15:42:06 -0300

    [master] feat: enable trust proxy for express app

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ import { apiRateLimiter } from './utils/middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', true);
+
   // CORS
   app.enableCors({
     origin: [
